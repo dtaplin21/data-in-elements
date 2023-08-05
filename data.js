4b6c2ce35b0a169d615f55e7bcc1267e45607367
Dom <dtaplin21@aol.com>
2023-08-05 10:33:55 -0700

    event

--- data.js
+++ data.js
@@ -1,1 +1,14 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+
+    const button = document.getElementById("add");
+
+    buttom.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const input = document.getElementById("name");
+        const select = document.getElementById("type");
+
+        console.log(input.value);
+        console.log(select.value)
+    });
+})
